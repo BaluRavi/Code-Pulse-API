c983183e7b6628bc2b035c3dcb09cf1918bc9d9c
Balu Ravi <balu89.rs@gmail.com>
2023-12-31 17:13:39 +0530

    Add project files.

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            var response = new CreateCategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ CodePulse.API/Repositories/Implementations/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interfaces;
+
+namespace CodePulse.API.Repositories.Implementations
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDBContext dBContext;
+
+        public CategoryRepository(ApplicationDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dBContext.Categories.AddAsync(category);
+            await dBContext.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
